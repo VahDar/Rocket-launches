48266a8610a1d99ad865b457cddeef6d18c40f2c
Vakhtang Daredzhanashvili <mymailbox14a@gmail.com>
2024-03-05 18:28:42 +0200

    setup appcoordinator and SceneDelegate

---
+++ TestProjectRocket/Application/AppCoordinator.swift
@@ -0,0 +1,37 @@
+//
+//  AppCoordinator.swift
+//  TestProjectRocket
+//
+//  Created by Vakhtang on 05.03.2024.
+//
+
+import UIKit
+
+protocol AppCoordinatorProtocol: Coordinator {
+    var window: UIWindow? { get }
+}
+
+class AppCoordinator: AppCoordinatorProtocol {
+    
+    let window: UIWindow?
+    var childCoordinator: [Coordinator] = []
+    weak var parentCoordinator: Coordinator?
+    var navigationController: UINavigationController
+    
+    init(window: UIWindow?) {
+        self.window = window
+        self.navigationController = UINavigationController()
+    }
+    
+    func start() {
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+        let vc = ViewController()
+        navigationController.pushViewController(vc, animated: true)
+        
+    }
+    
+    func finish() {
+        
+    }
+}
---
+++ TestProjectRocket/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  TestProjectRocket
+//
+//  Created by Vakhtang on 05.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .gray
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
