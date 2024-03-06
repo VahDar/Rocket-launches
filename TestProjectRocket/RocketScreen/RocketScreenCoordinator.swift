951ea150b1038c46884c837c1e17bcc0b4cefad8
Vakhtang Daredzhanashvili <mymailbox14a@gmail.com>
2024-03-06 20:35:13 +0200

    setum mainscreen coordinator

--- TestProjectRocket/RocketScreen/RocketScreenCoordinator.swift
+++ TestProjectRocket/RocketScreen/RocketScreenCoordinator.swift
@@ -5,4 +5,26 @@
 //  Created by Vakhtang on 06.03.2024.
 //
 
-import Foundation
+import UIKit
+
+final class RocketScreenCoordinator: Coordinator {
+    
+    var childCoordinator: [Coordinator] = []
+    var parentCoordinator: Coordinator?
+    var navigationController: UINavigationController
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func start() {
+        
+    }
+    
+    func finish() {
+        
+    }
+    
+    
+}
+
