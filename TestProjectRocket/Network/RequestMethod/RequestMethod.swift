b1be913e22f8849997582120c5c3ea601d452196
Vakhtang Daredzhanashvili <mymailbox14a@gmail.com>
2024-03-07 21:19:43 +0200

    add request method

---
+++ TestProjectRocket/Network/RequestMethod/RequestMethod.swift
@@ -0,0 +1,16 @@
+//
+//  RequestMethod.swift
+//  TestProjectRocket
+//
+//  Created by Vakhtang on 07.03.2024.
+//
+
+import Foundation
+
+enum RequestMethod: String {
+    case delete = "DELETE"
+    case get = "GET"
+    case patch = "PATCH"
+    case post = "POST"
+    case put = "PUT"
+}
