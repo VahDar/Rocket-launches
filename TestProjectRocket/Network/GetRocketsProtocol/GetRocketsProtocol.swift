afabb8c510f6d77ac1234983353a851c45b87f01
Vakhtang Daredzhanashvili <mymailbox14a@gmail.com>
2024-03-07 18:09:44 +0200

    GetRockets protocol

---
+++ TestProjectRocket/Network/GetRocketsProtocol/GetRocketsProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  GetRocketsProtocol.swift
+//  TestProjectRocket
+//
+//  Created by Vakhtang on 07.03.2024.
+//
+
+import Foundation
+
+
+protocol GetRocketsProtocol {
+    func getRockets() async throws -> RocketModel
+}
