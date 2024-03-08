bf3a2202935aa1f6293b205d8ede446d319f6271
Vakhtang Daredzhanashvili <mymailbox14a@gmail.com>
2024-03-08 12:59:30 +0200

    start setuo collectionViewCell

---
+++ TestProjectRocket/RocketScene/View/RocketCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  RocketCollectionViewCell.swift
+//  TestProjectRocket
+//
+//  Created by Vakhtang on 08.03.2024.
+//
+
+import Foundation
+import UIKit
+
+class RocketCollectionViewCell: UICollectionViewCell {
+    
+    // MARK - Properties
+    static let identifier = "CollectionViewCell"
+    
+    // MARK - Views
+    
+}
