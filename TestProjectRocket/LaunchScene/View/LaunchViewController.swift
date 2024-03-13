67d2b45ddb1955f96ffff5cc7e1872b63c81937c
Vakhtang Daredzhanashvili <mymailbox14a@gmail.com>
2024-03-13 15:47:32 +0200

    setup collectionview

--- TestProjectRocket/LaunchScene/View/LaunchViewController.swift
+++ TestProjectRocket/LaunchScene/View/LaunchViewController.swift
@@ -9,10 +9,25 @@ import UIKit
 
 class LaunchViewController: UIViewController {
 
+    //MARK: - Properties
+    var viewModel: LaunchSceneViewModelProtocol!
+    
+    private let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.itemSize = CGSize(width: 150, height: 100)
+        layout.minimumLineSpacing = 16
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = .clear
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.register(LaunchCollectionViewCell.self, forCellWithReuseIdentifier: LaunchCollectionViewCell.identifier)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        return collectionView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        viewModel = LaunchSceneViewModel()
     }
     
 
